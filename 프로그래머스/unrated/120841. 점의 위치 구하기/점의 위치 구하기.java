ebc777df31f00f8d9ea45b54845da26c38279bae
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2023-11-27 09:31:14 +0900

    [level unrated] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] dot) {
+        int answer = 
+            dot[0]>0 && dot[1]>0 ? 1 : 
+            dot[0]<0 && dot[1]<0 ? 3 : 
+            dot[0]<0 && dot[1]>0 ? 2 :
+            4 
+            ;
+        
+        return answer;
+    }
+}
