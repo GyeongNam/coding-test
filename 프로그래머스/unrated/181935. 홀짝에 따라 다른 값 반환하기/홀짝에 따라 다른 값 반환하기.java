9fba2f59728bc61e49504955c50bc3460f1745f4
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2023-11-27 09:41:00 +0900

    [level unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 0; i<=n; i++){
+            if(n%2==0){
+                if(i%2==0){
+                    answer += Math.pow(i,2);
+                }
+            }else{
+                if(i%2!=0){
+                    answer += i;
+                }
+            }
+        }
+        return answer;
+    }
+}
