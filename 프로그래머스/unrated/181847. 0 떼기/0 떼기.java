b122dbf74a4c008da4c270cf5853c258e8a51f89
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2023-11-27 10:08:38 +0900

    [level unrated] Title: 0 떼기, Time: 0.03 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String n_str) {
+        int txt = Integer.parseInt(n_str); 
+        String answer = Integer.toString(txt);
+        return answer;
+    }
+}
