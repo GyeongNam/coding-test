c800bd8d99a06b58f79de8ca99372cb7f0bb055b
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2023-11-27 10:12:47 +0900

    [level unrated] Title: 뒤에서 5등까지, Time: 0.51 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        num_list = Arrays.copyOfRange(num_list,0,5);
+        return num_list;
+    }
+}
