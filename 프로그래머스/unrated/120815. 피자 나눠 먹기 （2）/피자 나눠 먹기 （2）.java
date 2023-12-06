d886ac36ac75c4abf63dbf93a694a933d8b74ff6
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2023-12-06 14:10:28 +0900

    [level unrated] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/unrated/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,12 +1,6 @@
 class Solution {
     public int solution(int n) {
-        // int answer = 1;
-        // int piz = 6;
-        // while(piz%n != 0){
-        //     answer++;
-        //     piz += 6;
-        // }
-        // return answer;
+   
         
         int answer = 0;
         int counter = 1;
