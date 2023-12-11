53ca547b33ffc1bb6c5ca96998df41af620d3aa2
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2023-12-11 16:25:47 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 868 ms, Memory: 236480 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int size = Integer.parseInt(br.readLine());
+        for (int i = 0; i < size; i++) {
+            st = new StringTokenizer(br.readLine());
+            bw.write(Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())+ "\n");
+        }
+        bw.close();
+	}
+}
