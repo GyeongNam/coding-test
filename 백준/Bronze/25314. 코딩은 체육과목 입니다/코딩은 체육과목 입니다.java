62fc259b7bf0182647c9b0e671487ccbe3e69be5
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2023-12-11 16:01:03 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 216 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long size = sc.nextLong();
+        for(long i = 0; i<size/4; i++){
+            System.out.print("long ");
+        }
+        System.out.print("int");
+	}
+}
