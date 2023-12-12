4a70e820c44237c14554ae90318fe3823cba11e5
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2023-12-12 09:11:42 +0900

    [Bronze V] Title: A+B - 8, Time: 288 ms, Memory: 19124 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println("Case #"+(i+1)+": "+a+" + "+b+" = "+(a+b));
+        }
+	}
+}
