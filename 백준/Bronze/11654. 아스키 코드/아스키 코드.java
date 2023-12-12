eb693901bf3c2383fcce166d2744990e46574372
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2023-12-12 14:48:09 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char size = sc.next().charAt(0);
+        int num = size;
+        System.out.println(num);
+	}
+}
