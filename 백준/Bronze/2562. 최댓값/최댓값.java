a57440f05e75f8e9da328b80e870f506525d59d2
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2023-12-12 10:17:04 +0900

    [Bronze III] Title: 최댓값, Time: 208 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[9];
+        for (int i = 0; i < 9; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = 0;
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(max < arr[i]){
+                max = arr[i];
+                index = i+1;
+            }
+        }
+        System.out.println(max);
+        System.out.println(index);
+	}
+}
