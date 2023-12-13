f2953b3510f8051e93b483940c32fa6616601315
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2023-12-13 22:00:23 +0900

    [level 2] Title: 더 맵게, Time: 1405.55 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -14,5 +14,21 @@ class Solution {
             answer++;
         }
         return answer;
+        // 배열 풀이
+//         int answer = 0;
+//         for (int i = 0; i<scoville.length-1; i++){
+//             Arrays.sort(scoville);
+//             if (Arrays.stream(scoville).filter(a->a!=-1).min().getAsInt() >= K){
+//                 break;
+//             }
+//             scoville[i+1] = Math.min(scoville[i], scoville[i+1]) +  Math.max(scoville[i], scoville[i+1])*2;
+//             scoville[i] = -1;
+//             answer++;
+//         }
+ 
+//         if(scoville.length-1 == answer && scoville[scoville.length-1] < K){
+//             return -1;
+//         }
+//         return answer;
     }
 }
