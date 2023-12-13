b98d7d87b2fe726f82c65ad1356f39e733540257
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2023-12-13 22:50:31 +0900

    [level 2] Title: 더 맵게, Time: 1652.40 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
@@ -14,21 +15,40 @@ class Solution {
             answer++;
         }
         return answer;
-        // 배열 풀이
-//         int answer = 0;
-//         for (int i = 0; i<scoville.length-1; i++){
-//             Arrays.sort(scoville);
-//             if (Arrays.stream(scoville).filter(a->a!=-1).min().getAsInt() >= K){
-//                 break;
-//             }
-//             scoville[i+1] = Math.min(scoville[i], scoville[i+1]) +  Math.max(scoville[i], scoville[i+1])*2;
-//             scoville[i] = -1;
-//             answer++;
-//         }
- 
-//         if(scoville.length-1 == answer && scoville[scoville.length-1] < K){
-//             return -1;
-//         }
-//         return answer;
+        
+        // // 배열 풀이 효율성 탈락.
+        // int answer = 0;
+        // for (int i = 0; i<scoville.length-1; i++){
+        //     Arrays.sort(scoville);
+        //     if (Arrays.stream(scoville).filter(a->a!=-1).min().getAsInt() >= K){
+        //         break;
+        //     }
+        //     scoville[i+1] = scoville[i] + scoville[i+1]*2;
+        //     scoville[i] = -1;
+        //     answer++;
+        // }
+        // if(scoville.length-1 == answer && scoville[scoville.length-1] < K){
+        //     return -1;
+        // }
+        // return answer;
+        
+        // // 리스트 풀이 효율성 탈락.
+        // int answer = 0;
+        // List<Integer> list = new ArrayList<>();
+        // for(int n : scoville){
+        //     list.add(n);
+        // }
+        // while (Collections.min(list) < K){
+        //     if(list.size() < 2){
+        //         return -1;
+        //     }
+        //     Collections.sort(list);
+        //     list.add(list.get(0) + list.get(1)*2);
+        //     list.remove(0);
+        //     list.remove(0);
+        //     answer++;
+        // }
+        // System.out.println(answer);
+        // return answer;
     }
 }
