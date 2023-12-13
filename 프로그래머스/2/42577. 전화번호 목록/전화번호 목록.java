17babf967720c21e73a2afa11851537567435575
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2023-12-13 11:29:32 +0900

    [level 2] Title: 전화번호 목록, Time: 343.49 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -9,6 +9,14 @@ class Solution {
                 break;
             }
         }
+        //     for (int i = 0; i<phone_book.length; i++) {
+        //     for(int j = i+1; j<phone_book.length; j++){
+        //         if (phone_book[i].startsWith(phone_book[j]) || phone_book[j].startsWith(phone_book[i])) {
+        //             answer = false;
+        //             break;
+        //         }
+        //     }
+        // }
         return answer;
     }
 }
