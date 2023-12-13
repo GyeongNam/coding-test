ea71829fed2e632d86e3031a3c1f3416bd8f51e7
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2023-12-13 16:13:14 +0900

    [level 2] Title: 프로세스, Time: 0.68 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,32 +1,6 @@
 import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
-        // int answer = 0;
-        // int max = 0;
-        // int max_index = 0;
-        // for(int i = 0; i<priorities.length; i++){
-        //     if(max < priorities[i]){
-        //         max = priorities[i];
-        //         max_index = i;
-        //     }
-        // }
-        // int count = 0;
-        // int tmp = 0;
-        // while(true){
-        //     if(max_index == location){
-        //         count++;
-        //         break;
-        //     }else{
-        //         if(max_index<priorities.length){
-        //             max_index++;
-        //             count++;
-        //         }else if (max_index == priorities.length) {
-        //             max_index = 0;
-        //         }
-        //     }
-        // }
-        // answer = count;
-        // return answer;
         PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
         int answer = 0;
 
