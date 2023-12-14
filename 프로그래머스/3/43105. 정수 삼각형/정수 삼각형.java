52cba1b7808e615c7c9fa77930b5f2b5633c1362
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2023-12-14 15:34:01 +0900

    [level 3] Title: 정수 삼각형, Time: 7.61 ms, Memory: 61.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+                int[][] dp = new int [triangle.length][];
+        for(int i = 0; i<triangle.length; i++){
+            dp[i] = new int[triangle[i].length];
+            for (int j = 0; j < triangle[i].length; j++) {
+                dp[i][j] += triangle[i][j];
+            }
+        }
+
+        for(int i = 0; i<dp.length-1; i++){
+            for (int j = 0; j < triangle[i].length; j++) {
+                if(i == 0){
+                    dp[i+1][j] += dp[i][j];
+                    dp[i+1][j+1] += dp[i][j];
+                }else{
+                    if(dp[i+1][j] < dp[i][j] + triangle[i+1][j]){
+                        dp[i+1][j] = dp[i][j] + triangle[i+1][j];
+                    }
+                    dp[i+1][j+1] += dp[i][j];
+                }
+            }
+        }
+        answer = Arrays.stream(dp[dp.length-1]).max().getAsInt();
+        return answer;
+    }
+}
