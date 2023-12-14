16277035fccd279d2d6a058feb1506a0b9e9cc07
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2023-12-14 10:34:27 +0900

    [level 2] Title: 프로세스, Time: 0.69 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -22,4 +22,32 @@ class Solution {
         }
         return answer;
     }
+
+
+//     public int solution(int[] priorities, int location) {
+//         int answer = 0;
+//         Queue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+//             @Override
+//             public int compare(int[] o1, int[] o2) {
+//                 return o2[1] - o1[1];
+//             }
+//         });
+        
+//         List<int[]> polledList = new ArrayList<>();
+//         for (int i = 0; i < priorities.length; i++) {
+//             pq.offer(new int[]{i, priorities[i]});
+//         }
+//         int size = pq.size();
+//         int[] polled = new int[1];
+//         while(!pq.isEmpty()) {
+//             polledList.add(pq.poll());
+//         }
+//         for (int i = 0; i < polledList.size(); i++) {
+//             if (polledList.get(i)[0] == location) {
+//                 answer = i + 1;
+//             }
+//         }
+     
+//         return answer;
+//     }
 }
