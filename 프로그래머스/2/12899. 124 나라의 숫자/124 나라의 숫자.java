6acde1221a73cda01da421c3fa45b237d6520aa2
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2023-12-29 16:24:56 +0900

    [level 2] Title: 124 나라의 숫자, Time: 1.59 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -1,26 +1,34 @@
 import java.util.*;
 class Solution {
     public String solution(int n) {
-        int[] values = {0,1,2,4};
-        List<Integer> list = new ArrayList<>();
+//         int[] values = {0,1,2,4};
+//         List<Integer> list = new ArrayList<>();
 
-        while(n > 3){
-            int q = n/3;
-            int r = n%3;
-            if(r == 0){
-                q = q-1;
-                r = r+3;
-            }
-            n = q;
-            list.add(r);
-        }
+//         while(n > 3){
+//             int q = n/3;
+//             int r = n%3;
+//             if(r == 0){
+//                 q = q-1;
+//                 r = r+3;
+//             }
+//             n = q;
+//             list.add(r);
+//         }
 
-        if(n < 4){
-            list.add(n);
-        }
-        StringBuffer sb = new StringBuffer();
-        list.forEach(v -> sb.append(values[v]));
+//         if(n < 4){
+//             list.add(n);
+//         }
+//         StringBuffer sb = new StringBuffer();
+//         list.forEach(v -> sb.append(values[v]));
 
-        return sb.reverse().toString();
+//         return sb.reverse().toString();
+      String[] num = {"4","1","2"};
+      String answer = "";
+
+      while(n > 0){
+          answer = num[n % 3] + answer;
+          n = (n - 1) / 3;
+      }
+      return answer;
     }
 }
