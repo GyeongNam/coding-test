2c887bdc25371beb267a01d8a71fb41941723ee6
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2024-01-01 15:52:32 +0900

    [level 2] Title: 더 맵게, Time: 1416.44 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 class Solution {
     public int solution(int[] scoville, int K) {
+        // 우선큐풀이
         int answer = 0;
         Queue<Integer> q = new PriorityQueue<>();
         for(int n : scoville){
@@ -32,14 +33,14 @@ class Solution {
         // }
         // return answer;
         
-        // // 리스트 풀이 효율성 탈락.
+        // 리스트 풀이 효율성 탈락.
         // int answer = 0;
         // List<Integer> list = new ArrayList<>();
         // for(int n : scoville){
         //     list.add(n);
         // }
         // while (Collections.min(list) < K){
-        //     if(list.size() < 2){
+        //     if(list.size() == 1){
         //         return -1;
         //     }
         //     Collections.sort(list);
@@ -50,5 +51,30 @@ class Solution {
         // }
         // System.out.println(answer);
         // return answer;
+        
+        // TreeSet 억지 풀이 => 중복 값 때문에 실패
+//         int answer = 0;
+//         TreeSet<Integer> set = new TreeSet<>(
+//                 new Comparator<Integer>() {
+//                     @Override
+//                     public int compare(Integer o1, Integer o2) {
+//                         return o1.compareTo(o2);
+//                     }
+//                 }
+//         );
+//         for(int n : scoville){
+//             set.add(n);
+//         }
+//         while (set.first() < K){
+//             if(set.size() == 1){
+//                 return -1;
+//             }
+//             set.add(set.first() + set.higher(set.first())*2);
+//             set.remove(set.first());
+//             set.remove(set.first());
+//             answer++;
+            
+//         }
+//         return answer;
     }
 }
