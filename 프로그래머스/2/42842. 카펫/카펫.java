f0bfbe1ab0aef795a5a2d9431608d669d4a987af
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2024-01-04 12:42:48 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,18 +1,37 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        int num = brown + yellow;
-        for (int i = 3; i < num; i++) {
-            int j = num / i;
-            if (num % i == 0 && j >= 3) {
-                int col = Math.max(i, j);
-                int row = Math.min(i, j);
-                int center = (col - 2) * (row - 2);
+//         int[] answer = new int[2];
+//         int num = brown + yellow;
+//         for (int i = 3; i < num; i++) {
+//             int j = num / i;
+//             if (num % i == 0 && j >= 3) {
+//                 int col = Math.max(i, j);
+//                 int row = Math.min(i, j);
+//                 int center = (col - 2) * (row - 2);
 
-                if (center == yellow) {
-                    answer[0] = col;
-                    answer[1] = row;
-                    return answer;
+//                 if (center == yellow) {
+//                     answer[0] = col;
+//                     answer[1] = row;
+//                     return answer;
+//                 }
+//             }
+//         }
+//         return answer;
+        int[] answer = new int[2];
+        int carpet = brown + yellow;
+        for (int i = 3; i <= carpet; i++) {
+            int col = i;
+            int row = carpet / col; 
+ 
+            if (row < 3) {
+                continue;
+            }
+ 
+            if (row >= col) {
+                if ((row - 2) * (col - 2) == yellow) {
+                    answer[0] = row;
+                    answer[1] = col;
+                    break;
                 }
             }
         }
