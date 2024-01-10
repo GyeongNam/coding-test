20198a0c38979899983ed4ff07e0f0c7ca969e5e
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2024-01-10 16:23:40 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.54 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,21 +1,19 @@
 import java.util.*;
 class Solution {
-    public int[] solution(String[] operations) {
-    Queue<Integer> minHeap = new PriorityQueue<>(); 
-	Queue<Integer> maxHeap = new PriorityQueue<>();
-	for (int i = 0; i < operations.length; i++) {
-	    if (operations[i].charAt(0) == 'I') { 
-	        int data = Integer.parseInt(operations[i].split(" ")[1]);
-		    minHeap.add(data);
-		    maxHeap.add(-data); 
-	    } else if (!minHeap.isEmpty() && operations[i].equals("D 1")) { 
-		    minHeap.remove(-maxHeap.poll()); 
-        } else if (!minHeap.isEmpty() && operations[i].equals("D -1")) {  
-		    maxHeap.remove(-minHeap.poll()); 
+    public int[] solution(String[] operations) {   
+        TreeSet<Integer> ts = new TreeSet<>();
+        for (String opStr : operations) {
+        StringTokenizer st = new StringTokenizer(opStr);
+        if (st.nextToken().equals("I")) {
+            int value = Integer.parseInt(st.nextToken());
+            ts.add(value);
+            } else if (!ts.isEmpty() && opStr.equals("D 1")) {
+                ts.pollLast();
+            } else if (!ts.isEmpty() && opStr.equals("D -1")) {
+                ts.pollFirst();
+            }
         }
-	}
-        
-	return minHeap.isEmpty() ? new int[] {0, 0} : new int[] {-maxHeap.poll(), minHeap.poll()};
-    
+        return ts.isEmpty() ? new int[] {0, 0} : new int[] {ts.last(), ts.first()};
+
     }
 }
