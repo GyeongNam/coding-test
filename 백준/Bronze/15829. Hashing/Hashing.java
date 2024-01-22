a117bd3d5a753e91b1eff9af55eb937949765f18
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2024-01-22 09:14:35 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14220 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,19 @@
+import java.io.*;
+public class Main {
+	static final int M = 1234567891;
+	public static void main(String[] args) throws IOException {
+		BufferedReader bw = new BufferedReader(new InputStreamReader(System.in));
+		int L = Integer.parseInt(bw.readLine());
+		String str = new String(bw.readLine());
+		long sum = 0;
+		long pow = 1;
+		char[] arr = str.toCharArray();
+		for(int i = 0; i < L; i++)
+		{
+			sum += (arr[i] - 'a' + 1) * pow % M;
+			pow = pow * 31 % M;
+		}
+		long hash = sum % M;
+		System.out.println(hash);
+	}
+}
