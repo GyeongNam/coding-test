ae97100a2a0c021d6553d1cbdce71e889ac2cdf6
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2024-05-17 17:51:28 +0900

    [Silver V] Title: 다리 놓기, Time: 148 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            int result = 1;
+            for (int j = 0; j < N; j++) {
+                result = result * (M - j) / (j + 1);
+            }
+            System.out.println(result);
+        }
+    }
+}
