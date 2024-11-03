41472fbd4f2fd05936ef98bb1f5f70f63d87be81
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2024-11-03 23:17:22 +0900

    [level 0] Title: 안전지대, Time: 0.05 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -1,41 +1,33 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Solution {
     public int solution(int[][] board) {
         int n = board.length;
-        Map<String, Boolean> dangerMap = new HashMap<>();
+        boolean[][] dangerZone = new boolean[n][n];
         int safeCount = n * n;
-        
+
         int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
         int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
-        
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (board[i][j] == 1) {
-                    String minePosition = i + "," + j;
-
-                    if (!dangerMap.containsKey(minePosition)) {
-                        dangerMap.put(minePosition, true);
+                    if (!dangerZone[i][j]) {
+                        dangerZone[i][j] = true;
                         safeCount--;
                     }
-                    
+
                     for (int d = 0; d < 8; d++) {
                         int nx = i + dx[d];
                         int ny = j + dy[d];
-                        
-                        if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-                            String dangerPosition = nx + "," + ny;
-                            if (!dangerMap.containsKey(dangerPosition)) {
-                                dangerMap.put(dangerPosition, true);
-                                safeCount--;
-                            }
+
+                        if (nx >= 0 && nx < n && ny >= 0 && ny < n && !dangerZone[nx][ny]) {
+                            dangerZone[nx][ny] = true;
+                            safeCount--;
                         }
                     }
                 }
             }
         }
-        
+
         return safeCount;
     }
 }
