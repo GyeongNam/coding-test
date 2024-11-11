b179b7de6a74935dbf909c6a0344426f70d2c716
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2024-11-12 00:58:21 +0900

    [level 0] Title: 평행, Time: 0.04 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -1,36 +1,22 @@
-import java.util.*;
-
 class Solution {
     private boolean Giulgi(int[] dot1, int[] dot2, int[] dot3, int[] dot4) {
-        int dx1 = dot2[0] - dot1[0];
-        int dy1 = dot2[1] - dot1[1];
-        int dx2 = dot4[0] - dot3[0];
-        int dy2 = dot4[1] - dot3[1];
-        return (dy1 * dx2) == (dy2 * dx1);
+        return (dot2[1] - dot1[1]) * (dot4[0] - dot3[0]) == (dot4[1] - dot3[1]) * (dot2[0] - dot1[0]);
     }
+
     public int solution(int[][] dots) {
-        List<int[]> combinations = new ArrayList<>();
-        
-        for (int i = 0; i < dots.length; i++) {
+        for (int i = 0; i < dots.length - 1; i++) {
             for (int j = i + 1; j < dots.length; j++) {
-                combinations.add(new int[]{i, j});
-            }
-        }
-
-        for (int i = 0; i < combinations.size(); i++) {
-            for (int j = i + 1; j < combinations.size(); j++) {
-                int[] pair1 = combinations.get(i);
-                int[] pair2 = combinations.get(j);
-                
-                if (pair1[0] != pair2[0] && pair1[0] != pair2[1] && pair1[1] != pair2[0] && pair1[1] != pair2[1]) {
-
-                    if (Giulgi(dots[pair1[0]], dots[pair1[1]], dots[pair2[0]], dots[pair2[1]])) {
-                        return 1;
+                for (int k = 0; k < dots.length - 1; k++) {
+                    for (int l = k + 1; l < dots.length; l++) {
+                        if (i != k && i != l && j != k && j != l) {
+                            if (Giulgi(dots[i], dots[j], dots[k], dots[l])) {
+                                return 1; 
+                            }
+                        }
                     }
                 }
             }
         }
-
         return 0;
     }
 }
