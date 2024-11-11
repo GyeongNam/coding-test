84846a70e952db8505e5d8237ef2cdd4c3c8a749
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2024-11-12 00:04:29 +0900

    [level 0] Title: 평행, Time: 0.04 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -1,14 +1,14 @@
 import java.util.*;
 
 class Solution {
-    private boolean isGiulgi(int[] dot1, int[] dot2, int[] dot3, int[] dot4) {
+    private boolean Giulgi(int[] dot1, int[] dot2, int[] dot3, int[] dot4) {
         return (dot2[1] - dot1[1]) * (dot4[0] - dot3[0]) == (dot4[1] - dot3[1]) * (dot2[0] - dot1[0]);
     }
     public int solution(int[][] dots) {
         ArrayList arr = new ArrayList<>();
-        if (isGiulgi(dots[0], dots[1], dots[2], dots[3])) return 1;
-        if (isGiulgi(dots[0], dots[2], dots[1], dots[3])) return 1;
-        if (isGiulgi(dots[0], dots[3], dots[1], dots[2])) return 1;
+        if (Giulgi(dots[0], dots[1], dots[2], dots[3])) return 1;
+        if (Giulgi(dots[0], dots[2], dots[1], dots[3])) return 1;
+        if (Giulgi(dots[0], dots[3], dots[1], dots[2])) return 1;
         return 0;
     }
 }
