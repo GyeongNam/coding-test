a32d87d9df624aeb7783c2c957b45bcaac29c7ed
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2024-11-12 00:12:27 +0900

    [level 0] Title: 평행, Time: 0.05 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -2,13 +2,35 @@ import java.util.*;
 
 class Solution {
     private boolean Giulgi(int[] dot1, int[] dot2, int[] dot3, int[] dot4) {
-        return (dot2[1] - dot1[1]) * (dot4[0] - dot3[0]) == (dot4[1] - dot3[1]) * (dot2[0] - dot1[0]);
+        int dx1 = dot2[0] - dot1[0];
+        int dy1 = dot2[1] - dot1[1];
+        int dx2 = dot4[0] - dot3[0];
+        int dy2 = dot4[1] - dot3[1];
+        return (dy1 * dx2) == (dy2 * dx1);
     }
     public int solution(int[][] dots) {
-        ArrayList arr = new ArrayList<>();
-        if (Giulgi(dots[0], dots[1], dots[2], dots[3])) return 1;
-        if (Giulgi(dots[0], dots[2], dots[1], dots[3])) return 1;
-        if (Giulgi(dots[0], dots[3], dots[1], dots[2])) return 1;
+        List<int[]> combinations = new ArrayList<>();
+        
+        for (int i = 0; i < dots.length; i++) {
+            for (int j = i + 1; j < dots.length; j++) {
+                combinations.add(new int[]{i, j});
+            }
+        }
+
+        for (int i = 0; i < combinations.size(); i++) {
+            for (int j = i + 1; j < combinations.size(); j++) {
+                int[] pair1 = combinations.get(i);
+                int[] pair2 = combinations.get(j);
+                
+                if (pair1[0] != pair2[0] && pair1[0] != pair2[1] && pair1[1] != pair2[0] && pair1[1] != pair2[1]) {
+
+                    if (Giulgi(dots[pair1[0]], dots[pair1[1]], dots[pair2[0]], dots[pair2[1]])) {
+                        return 1;
+                    }
+                }
+            }
+        }
+
         return 0;
     }
 }
