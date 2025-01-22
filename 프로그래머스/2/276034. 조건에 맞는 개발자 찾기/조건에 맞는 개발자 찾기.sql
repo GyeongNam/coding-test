ad40556d809d717354a27516737c52795b92a5bb
GyeongNam <63902992+GyeongNam@users.noreply.github.com>
2025-01-23 00:06:04 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,17 @@
+-- 코드를 작성해주세요
+
+select ID, EMAIL, FIRST_NAME, LAST_NAME
+from DEVELOPERS
+where 
+SKILL_CODE & (
+    select CODE
+    from SKILLCODES
+    where NAME = 'Python'
+   )
+OR
+SKILL_CODE & (
+    select CODE
+    from SKILLCODES
+    where NAME = 'C#'
+    )
+order by ID
